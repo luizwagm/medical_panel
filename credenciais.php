4597ca137f764c17799c6f7d22e03bbf134cac16
Luiz Augusto <luizwagm@gmail.com>
2022-07-19 21:39:56 -0300

    ordem das credenciais

--- credenciais.php
+++ credenciais.php
@@ -76,11 +76,11 @@ curl_close($ch);
                                                                     </thead>
                                                                     <tbody>
                                                                         <?php
-                                                                            foreach(json_decode($result) as $value) {
+                                                                            foreach(json_decode($result) as $key => $value) {
                                                                                 $validDel = ! empty($value->delete);
                                                                         ?>
                                                                         <tr <?php echo $validDel ? 'style="background-color: #FCC0C0; color:#000 !important; transparent: 0.5"' : ''; ?>>
-                                                                            <td><?php echo $validDel ? '<span class="badge" style="background-color: #ae0001; font-size: 10px">Não conecta no sistema!</span>' : ''; ?></td>
+                                                                            <td><?php echo $key; ?> - <?php echo $validDel ? '<span class="badge" style="background-color: #ae0001; font-size: 10px">Não conecta no sistema!</span>' : ''; ?></td>
                                                                             <td><?php echo $value->ordem; ?></td>
                                                                             <td><?php echo $value->cpf; ?></td>
                                                                             <td><?php echo $value->senha; ?></td>
