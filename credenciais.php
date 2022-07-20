0451bba876759a50a53e813d1efe662c81fa72a1
Luiz Augusto <luizwagm@gmail.com>
2022-07-19 21:51:34 -0300

    quantidades

--- credenciais.php
+++ credenciais.php
@@ -18,6 +18,16 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 $result = curl_exec($ch);
 curl_close($ch);
 
+$cadastrosCredenciais = 0;
+$errosCredenciais = 0;
+foreach(json_decode($result) as $key => $value) {
+    if (! empty($value->delete)) {
+        $errosCredenciais++;
+    } else {
+        $cadastrosCredenciais++;
+    }
+}
+
 ?>
     <body>
         <div id="app">
@@ -38,23 +48,65 @@ curl_close($ch);
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-md-4">
-                                        <form action="./actions/storeCredentials.php" method="post">
-                                            <div class="form-group">
-                                                <label for="basicInput">CPF</label>
-                                                <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Insira o CPF">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <form action="./actions/storeCredentials.php" method="post">
+                                                    <div class="form-group">
+                                                        <label for="basicInput">CPF</label>
+                                                        <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Insira o CPF">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="basicInput">Senha</label>
+                                                        <input type="text" class="form-control" id="senha" name="senha" placeholder="Insira a senha">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="basicInput">Nome do paciente</label>
+                                                        <input type="text" class="form-control" id="nome_paciente" name="nome_paciente" placeholder="Insira o nome do paciente">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <button class="btn btn-primary">Salvar</button>
+                                                    </div>
+                                                </form>
                                             </div>
-                                            <div class="form-group">
-                                                <label for="basicInput">Senha</label>
-                                                <input type="text" class="form-control" id="senha" name="senha" placeholder="Insira a senha">
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="card">
+                                                    <div class="card-body px-3 py-4-5">
+                                                        <div class="row">
+                                                            <div class="col-md-4">
+                                                                <div class="stats-icon purple">
+                                                                    <i class="iconly-boldShow"></i>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-8">
+                                                                <h6 class="text-muted font-semibold">Credenciais cadastradas</h6>
+                                                                <h6 class="font-extrabold mb-0"><?php echo $cadastrosCredenciais; ?></h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div class="form-group">
-                                                <label for="basicInput">Nome do paciente</label>
-                                                <input type="text" class="form-control" id="nome_paciente" name="nome_paciente" placeholder="Insira o nome do paciente">
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="card">
+                                                    <div class="card-body px-3 py-4-5">
+                                                        <div class="row">
+                                                            <div class="col-md-4">
+                                                                <div class="stats-icon red">
+                                                                    <i class="iconly-boldBookmark"></i>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-8">
+                                                                <h6 class="text-muted font-semibold">Credenciais com erro</h6>
+                                                                <h6 class="font-extrabold mb-0"><?php echo $errosCredenciais; ?></h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div class="form-group">
-                                                <button class="btn btn-primary">Salvar</button>
-                                            </div>
-                                        </form>
+                                        </div>
+
                                     </div>
                                     <div class="col-md-8">
                                         <section class="section">
