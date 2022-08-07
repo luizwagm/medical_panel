6050743cfecf9d7d684192c54611890de136cdb9
Luiz Augusto <luiz.augusto@datum.inf.br>
2022-08-07 16:38:44 -0300

    fix

--- includes/menu.php
+++ includes/menu.php
@@ -2,36 +2,30 @@
     <ul class="menu">      
         <li class="sidebar-item  has-sub <?php if (MENU == 'lista') { echo 'active'; } ?>">
             <a href="#" class='sidebar-link'>
-                <i class="bi bi-grid-fill"></i>
                 <span>Listas de reembolso</span>
             </a>
-            <ul class="submenu ">
-                <?php if(in_array('Bradesco', $_SESSION['payload']->modules)) { ?><li class="submenu-item ">
-                    <a href="./lista-bradesco.php">Bradesco</a>
-                </li><?php } ?>
-                <?php if(in_array('Amil', $_SESSION['payload']->modules)) { ?><li class="submenu-item ">
-                    <a href="./lista-amil.html">Amil</a>
-                </li><?php } ?>
-            </ul>
         </li>
+        <?php if(in_array('Bradesco', $_SESSION['payload']->modules)) { ?><li class="sidebar-item ">
+            <a href="./lista-bradesco.php">Bradesco</a>
+        </li><?php } ?>
+        <?php if(in_array('Amil', $_SESSION['payload']->modules)) { ?><li class="sidebar-item ">
+            <a href="./lista-amil.html">Amil</a>
+        </li><?php } ?>
 
         <li class="sidebar-item <?php if (MENU == 'credenciais') { echo 'active'; } ?>">
             <a href="./credenciais.php" class='sidebar-link'>
-                <i class="bi bi-people-fill"></i>
                 <span>Credenciais de pacientes</span>
             </a>
         </li> 
 
         <li class="sidebar-item <?php if (MENU == 'protocolos') { echo 'active'; } ?>">
             <a href="./protocolos.php" class='sidebar-link'>
-                <i class="bi bi-xorg"></i>
                 <span>Protocolos</span>
             </a>
         </li> 
 
         <li class="sidebar-item <?php if (MENU == 'configuracoes') { echo 'active'; } ?>">
             <a href="./configuracoes.php" class='sidebar-link'>
-                <i class="bi bi-xorg"></i>
                 <span>Configurações</span>
             </a>
         </li> 
